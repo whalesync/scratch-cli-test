ca2be8257c633bef301e929fc43875dc42e79edf
Ivan Dimitrov <ivan@whalesync.com>
2026-03-02 14:32:32 +0000

    [gitoxide] fix gitoxide warnings
    ## Change description
    
    * Cemp fix for rust warnings - commented out some structures that will be used in the next phase of the refactor. The next phase is storing the repos in a deeper folder structure orgId - workbookId - connectionId. For now the repos are flat with the same 3 id's part of the repo name
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- scratch-git-2/src/state.rs
+++ scratch-git-2/src/state.rs
@@ -6,12 +6,15 @@ use dashmap::DashMap;
 use crate::config::Config;
 use crate::git::lock::WriteLockManager;
 
-/// Separator used in V2 composite repo IDs: `{orgId}--{workbookId}--{connAccountId}`
-const V2_ID_SEPARATOR: &str = "--";
+// TODO(v2-routes): wire up V2 repo routing in the final phase
+// /// Separator used in V2 composite repo IDs: `{orgId}--{workbookId}--{connAccountId}`
+// const V2_ID_SEPARATOR: &str = "--";
 
 #[derive(Clone)]
 pub struct AppState {
     pub repos_dir: PathBuf,
+    // TODO(v2-routes): used in final phase when routes are wired to V2 repo paths
+    #[allow(dead_code)]
     pub repos_v2_dir: PathBuf,
     pub build_version: String,
     /// Map of repo_id → GC start timestamp (millis since epoch)
@@ -35,20 +38,21 @@ impl AppState {
         self.repos_dir.join(format!("{}.git", repo_id))
     }
 
-    /// V2 repo path from a composite ID `{orgId}--{workbookId}--{connAccountId}`.
-    /// Maps to `{repos_v2_dir}/{orgId}/{workbookId}/{connAccountId}.git`.
-    /// Falls back to treating the whole id as a flat name if it doesn't contain the separator.
-    pub fn repo_path_v2(&self, composite_id: &str) -> PathBuf {
-        let parts: Vec<&str> = composite_id.splitn(3, V2_ID_SEPARATOR).collect();
-        match parts.as_slice() {
-            [org_id, workbook_id, conn_id] => self
-                .repos_v2_dir
-                .join(org_id)
-                .join(workbook_id)
-                .join(format!("{}.git", conn_id)),
-            _ => self
-                .repos_v2_dir
-                .join(format!("{}.git", composite_id)),
-        }
-    }
+    // TODO(v2-routes): used in final phase when routes are wired to V2 repo paths
+    // /// V2 repo path from a composite ID `{orgId}--{workbookId}--{connAccountId}`.
+    // /// Maps to `{repos_v2_dir}/{orgId}/{workbookId}/{connAccountId}.git`.
+    // /// Falls back to treating the whole id as a flat name if it doesn't contain the separator.
+    // pub fn repo_path_v2(&self, composite_id: &str) -> PathBuf {
+    //     let parts: Vec<&str> = composite_id.splitn(3, V2_ID_SEPARATOR).collect();
+    //     match parts.as_slice() {
+    //         [org_id, workbook_id, conn_id] => self
+    //             .repos_v2_dir
+    //             .join(org_id)
+    //             .join(workbook_id)
+    //             .join(format!("{}.git", conn_id)),
+    //         _ => self
+    //             .repos_v2_dir
+    //             .join(format!("{}.git", composite_id)),
+    //     }
+    // }
 }
